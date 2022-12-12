e77a7fade565a3fb4d6a35a1d9e94b200679323b
Kings Mkumbwa <kingsmkumbwa@gmail.com>
2022-12-12 05:52:22 -0800

    c program for puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
+
+/**
+ * main entry point 
+ * return always 0
+ * this program will print string with use of put 
+ */
 int main(void)
 {
-	
 	puts("Programming is like building a multilingual puzzle\n");	
-	return 0;
+	return (0);
 }
-
