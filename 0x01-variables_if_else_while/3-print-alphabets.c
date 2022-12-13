0a92fca73b1c9c9599dd4483a995ca1299d294ad
Kings Mkumbwa <kingsmkumbwa@gmail.com>
2022-12-12 16:07:47 -0800

    plus upper cases put char things

---
+++ 0x01-variables_if_else_while/3-print-alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry leve
+ *
+ * Return: Always 0 (success)
+ *
+ * this program prints alphabet plus new line
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		putchar(x);
+	}
+	for (x = 'A'; x <= 'Z'; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
