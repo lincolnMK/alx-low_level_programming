427504207a4bf8d32e5e260957917cca4d3f1d03
Kings Mkumbwa <kingsmkumbwa@gmail.com>
2022-12-12 16:32:59 -0800

    same add print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry leve
+ *
+ * Return: Always 0 (success)
+ *
+ * this program prints numbers plus new line
+ */
+int main(void)
+{
+	int x;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
