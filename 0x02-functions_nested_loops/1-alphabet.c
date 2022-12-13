eae8b1dd38f53672ecb3c4d09e8610dbfb40b047
Kings Mkumbwa <kingsmkumbwa@gmail.com>
2022-12-13 12:57:01 -0800

    stuff

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,6 @@
 #include "main.h"
 /**
- * main - Entry point
- *
- * Return: Always 0 (succes)
- *
- * print_alphabet: this program print th alphabet
+  * print_alphabet - this program print th alphabeit
  */
 void print_alphabet(void)
 	{
