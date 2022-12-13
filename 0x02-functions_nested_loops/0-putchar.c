776c24daacbbe5e257b88ba335a63edc6073f998
Kings Mkumbwa <kingsmkumbwa@gmail.com>
2022-12-13 12:01:37 -0800

    finally some progress

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,5 +9,13 @@
 int main(void)
 {
 	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	-putchar("t");
+	_putchar('c');
+	_putchar('h');
+	_putchar("a");
+	_putchar("r");
+	_putchar("\n");
 	return (0);
 }
