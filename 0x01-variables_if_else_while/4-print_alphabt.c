f78c9da9bc155cf8f73d630b537288ccf520d8a4
Kings Mkumbwa <kingsmkumbwa@gmail.com>
2022-12-12 16:21:27 -0800

    ommit chars

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -13,7 +13,15 @@ int main(void)
 
 	for (x = 'a'; x <= 'z'; x++)
 	{
-		if((x != 'e') || (x != 'q'))
+		if (x == 'e')
+		{
+			x++;
+		}
+		else if (x == 'q')
+		{
+			x++;
+		}
+		else 
 		{
 			putchar(x);
 		}
