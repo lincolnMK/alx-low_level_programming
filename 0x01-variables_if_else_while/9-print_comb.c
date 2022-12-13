02eff47d79daef29c98f52054de6c150210b5c56
Kings Mkumbwa <kingsmkumbwa@gmail.com>
2022-12-12 18:30:21 -0800

    chars

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,11 +14,16 @@ int main(void)
 	for (x = '0'; x <= '9'; x++)
 	{
 		putchar(x);
-		while(x == 9)
-		continue;
+		if (x >= '9')
+		{
+			continue;
+		}
+		else if (x < '9')
+		{
 			putchar(',');
 			putchar(' ');
-	
+		}
+
 	}
 	putchar('\n');
 	return (0);
