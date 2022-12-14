d0dc08c6f8e9dd07a2663134752ae800bf9a78ee
Kings Mkumbwa <kingsmkumbwa@gmail.com>
2022-12-14 10:31:03 -0800

    getting there

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -23,11 +23,17 @@ void print_to_98(int n)
 	else if (n > 98)
 	{
 		for (i = n; i >= 98; i--)
-		{
+		{  if ( i> 9)
+                                {
 			_putchar((i / 10) + '0');
+				}
+
 			_putchar((i % 10) + '0');
-				_putchar(',');
-				_putchar(' ');
+ while (i==98)
+                       {continue;
+                       }	
+			_putchar(',');
+			_putchar(' ');
 		}
 		_putchar('\n');
 	}
