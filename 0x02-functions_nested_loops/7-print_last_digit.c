7b4f770521fc98847deecfbd8505fe335d165109
Kings Mkumbwa <kingsmkumbwa@gmail.com>
2022-12-14 07:14:45 -0800

    final 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,16 +1,15 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- i* print_last_digit - prints absolute value
+ * print_last_digit - prints absolute value
  * @n : first operancd
  * Return: Always 0 (success)
  */
 int print_last_digit(int n)
 {
 	int ld;
-	ld = abs(n % 10);
 	
+	ld = abs(n % 10);
 	_putchar((ld % 10) + '0');
-
 	return (ld);
 }
