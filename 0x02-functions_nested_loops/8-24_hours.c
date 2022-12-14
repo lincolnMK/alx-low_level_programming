5e04b2e701cff6bb1126bdfad51a0e10982f5eff
Kings Mkumbwa <kingsmkumbwa@gmail.com>
2022-12-14 07:46:23 -0800

    bawa

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+/*
+ * jack_bauer - this prints the hours
+ * Return : will return void
+ *
+ */
+void jack_bauer(void)
+{
+	int i;
+
+	for (i = 0; i <= 23; i++)
+	{
+		int j;
+
+		for (j = 0; j <= 59; j++)
+		{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(':');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10)+ '0');
+			_putchar('\n');
+		}
+	}
+	return (0);
+}
