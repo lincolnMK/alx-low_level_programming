c3159272c61fba9e321b7472efa44e37c6e4c5dc
Kings Mkumbwa <kingsmkumbwa@gmail.com>
2022-12-15 05:18:52 -0800

    commenting

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#inlcude <stdio.h>
+
+/**
+* main - causes an infitnite loop
+* Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+
+	//while (1 < 10)
+	{
+		putchar(i);
+	}
+
+	printf("Infinite loop avoided! \\o/\n");
+	
+	return (0);
+}
