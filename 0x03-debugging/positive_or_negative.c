ca9807df083ffb787ca222eaa2ba1e87256d121f
Kings Mkumbwa <kingsmkumbwa@gmail.com>
2022-12-15 03:11:07 -0800

    done testing

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -8,24 +8,18 @@
  *     *
  *      * this program will decide negative or positive
  *       */
-int main(void)
+void positive_or_negative(int n)
 {
-		int n;
-
-			srand(time(0));
-				n = rand() - RAND_MAX / 2;
-
-					if (n < 0)
-							{
-										printf("%d is negative\n", n);
-											}
-						else if (n > 0)
-								{
-											printf("%d is positive\n", n);
-												}
-							else
-									{
-												printf("%d is zero\n", n);
-													}
-								return (0);
+	if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	else if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else
+	{
+	printf("%d is zero\n", n);
+	}
 }
