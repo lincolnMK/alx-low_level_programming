c9a6af0a9a7bc3da6395965fa5c136f7df153903
Kings Mkumbwa <kingsmkumbwa@gmail.com>
2022-12-15 15:03:49 -0800

    is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isupper - checks if chr is upper case
+ * @c: first operand
+ */
+int _isupper(int c)
+{
+	if (isupper(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <ctype.h>
+
+/* check is char is upper case */
+int _isupper(int c);
+
+#endif
