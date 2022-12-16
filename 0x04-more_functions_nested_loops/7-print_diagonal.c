49d818f687cf38b2f10009dab774a7cdea65f992
Kings Mkumbwa <kingsmkumbwa@gmail.com>
2022-12-15 17:38:00 -0800

    for inits

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -15,11 +15,11 @@ void print_diagonal(int n)
 	{
 		int i;
 
-		for (i = 0; i <= n; i++)
+		for (i = 0; i <= (n - 1); i++)
 		{
 			int j;
 
-			for (j = 0; j <= i; j++)
+			for (j = 0; j <= (i - 1); j++)
 			{
 				_putchar(' ');
 			}
