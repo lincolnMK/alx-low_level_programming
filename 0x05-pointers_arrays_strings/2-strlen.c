4bd091376fe536d82d550a57d2bae960b41b7907
lincolnMK <kingsmkumbwa@gmail.com>
2022-12-19 15:48:17 -0800

    stingLneght

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - finds lenth of string
+ * @s: of pointer
+ * Return: 0 always
+ */
+int _strlen(char *s)
+{
+	int k;
+
+	while (s[k] != '\0')
+	{
+		k++;
+	}
+
+	return (k);
+}
