c394d889d11f81730ae8f1c321a32ad64ead92ec
lincolnMK <kingsmkumbwa@gmail.com>
2022-12-19 14:43:39 -0800

    swapp

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * swap_int - swaps em like
+ * @a: first operand
+ * @b: second operand
+ * Return: always 0
+ */
+
+void swap_int(int *a, int *b)
+{
+	int *tmp;
+	
+	*tmp = *a;
+	a* = *b;
+	*b =*tmp;
+}
