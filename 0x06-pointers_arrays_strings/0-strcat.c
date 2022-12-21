9b53ee0f2699d7855b45dcdab96046ad9b2c31cd
lincolnMK <kingsmkumbwa@gmail.com>
2022-12-20 19:38:50 -0800

    wierd

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcat - concatnates
+ * @dest: final
+ * @src: starto
+ * Return: always 0
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, k;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
+	for (k = 0; (dest[i+n] = *src++) != '\0'; k++)
+	{
+	}
+	return (dest);
+}
+
