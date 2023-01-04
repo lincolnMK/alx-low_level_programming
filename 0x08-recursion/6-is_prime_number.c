d3a49fd3861bd058d168c57a7ea21088464cae67
lincolnMK <kingsmkumbwa@gmail.com>
2023-01-04 06:13:29 -0800

    6th

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * _assess - recursive loopings
+ * @num : number
+ * @i : iteratring number
+ * Return: 1  or 0
+ */
+
+int _assess(int num, int i)
+{
+	if (i == (num - 1))
+	{
+		return (1);
+	}
+	else if ((num % i) == 0)
+	{
+		return (0);
+	}
+	if ((num % i) != 0)
+	{
+		return (_assess(num, i + 1));
+	}
+	return (0);
+}
+
+/*
+ * is_prime_number - finds prime numbers
+ * return: primers numbers
+ * @n: first operando
+ */
+int is_prime_number(int n)
+{
+	int i;
+
+	i = 2;
+
+	if (n < 2)
+	{
+		return (0);
+	}
+	if (n == 2)
+	{
+		return (1);
+	}
+	return (_assess(n, i));
+
+}
