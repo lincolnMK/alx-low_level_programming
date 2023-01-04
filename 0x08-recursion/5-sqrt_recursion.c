06446770508fc78adb045d04b94f37dd9835999b
lincolnMK <kingsmkumbwa@gmail.com>
2023-01-04 05:51:07 -0800

    trying_again

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -4,7 +4,7 @@
  * _assess - improvised fucntioin
  * Return: the square roo of the given number
  * @n: the number that needs the root of the saure
- * @i: another operando 
+ * @i: another operando
  */
 
 	int _assess(int i, int n)
@@ -30,7 +30,7 @@ int _sqrt_recursion(int n)
 {
 	int i = 0;
 
-	if (i < 0;)
+	if (i < 0)
 	{
 		return (-1);
 	}
