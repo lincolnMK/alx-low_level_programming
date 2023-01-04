e54824ba0376111cd7905aa50f6a06ac56ff9f34
lincolnMK <kingsmkumbwa@gmail.com>
2023-01-04 05:48:24 -0800

    trying

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
  * _sqrt_recursion - find the square root of the prvide number
- * _assess - improvise
+ * _assess - improvised fucntioin
  * Return: the square roo of the given number
  * @n: the number that needs the root of the saure
+ * @i: another operando 
  */
-int _sqrt_recursion(int n)
-{
+
 	int _assess(int i, int n)
 	{
 		if ((n == 0) || (n == 1))
@@ -16,13 +16,27 @@ int _sqrt_recursion(int n)
 
 		else if ((i * i) == n)
 		{
-			return (n);
+			return (i);
 		}
 		else if (i * i < n)
 		{
 			return (_assess(i + 1, n));
 		}
 		return (-1);
+		return (-1);
 	}
-	return (-1);
+
+int _sqrt_recursion(int n)
+{
+	int i = 0;
+
+	if (i < 0;)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (_assess(i, n));
+	}
 }
+
