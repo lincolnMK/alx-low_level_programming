33e3eb65f5b23c4570999e511425622984a5b8f7
lincolnMK <kingsmkumbwa@gmail.com>
2023-01-06 12:46:51 -0800

    static lib

--- 0x09-static_libraries/3-islower.c
+++ 0x09-static_libraries/3-islower.c
@@ -1,18 +1,3 @@
 #include"main.h"
-#include<stdio.h>
-#include<ctype.h>
-/**
- * _islower -prints if case is lower or upper
- *
- * Return: Always 0 (success)
- * @c: first input value int
- */
 int _islower(int c)
-{
-	if (islower(c))
-	{
-		return (1);
-	}
-	else
-		return (0);
-}
+{}
--- 0x09-static_libraries/4-isalpha.c
+++ 0x09-static_libraries/4-isalpha.c
@@ -1,18 +1,3 @@
 #include"main.h"
-#include<stdio.h>
-#include<ctype.h>
-/**
- * _isalpha - prints if case is lower or upper
- *
- * Return: Always 0 (success)
- * @c: first input value int
- */
 int _isalpha(int c)
-{
-	if (isalpha(c))
-	{
-		return (1);
-	}
-	else
-		return (0);
-}
+{}
