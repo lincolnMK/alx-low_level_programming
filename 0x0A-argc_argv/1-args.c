c3b7225e4785198eedbb666e8ed9e934195b188a
lincolnMK <kingsmkumbwa@gmail.com>
2023-01-08 01:08:57 -0800

    print number of arrgs passed

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints name of program
+ * @argc: count of args
+ * @argv: array of them ergs
+ * return: 0 always
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
