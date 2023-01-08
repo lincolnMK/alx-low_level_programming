985076834f683e23e439e5bd9774571cd4465d07
lincolnMK <kingsmkumbwa@gmail.com>
2023-01-08 02:12:03 -0800

    atoi

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints name of program
+ * @argc: count of args
+ * @argv: array of them ergs
+ * return: 0 always
+ */
+int main(int argc, char	*argv[])
+{
+	if ((argc == 1) || (argc == 2))
+	{
+		printf("Error\n");
+	}
+	else
+	{
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	}
+	return (0);
+}
