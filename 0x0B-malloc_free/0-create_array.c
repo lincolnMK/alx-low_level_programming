8f8e8cd2cfe422b5dcf4bac0bf9ade527df95264
lincolnMK <kingsmkumbwa@gmail.com>
2023-01-10 14:56:48 -0800

    malloc wavuta

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,11 +15,11 @@ char *create_array(unsigned int size, char c)
 	}
 	else
 	{
-		int i;
+		unsigned int i;
 
-		for (i = 0; i < size; i++)
+		for (i = 0; i <= size; i++)
 		{
-			array[size] = c;
+			array[i] = c;
 		}
 		return (array);
 	}
