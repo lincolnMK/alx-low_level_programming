173adbf9a1b841d1e2d8ac814c91c9cff1449b11
lincolnMK <kingsmkumbwa@gmail.com>
2023-01-12 05:39:38 -0800

    white spacess

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,25 +1,27 @@
 #include "main.h"
 /**
- *
+ * rev_string - the function name
+ * @s: the operand
+ * Return: NULL 0
  */
 void rev_string(char *s)
 {
 	int i, j, len;
-	char tmp;	
+
+	char tmp;
+
 	len = 0;
-		
+
 	while (s[len] != '\0')
 	{
 		len++;
 	}
-	j = len -1;
+	j = len - 1;
 
 	for (i = 0; (i < (len / 2)); i++)
 	{
 		tmp = s[i];
 		s[i] = s[j];
-		s[j--] =tmp;
+		s[j--] = tmp;
 	}
-	
-	 
 }
