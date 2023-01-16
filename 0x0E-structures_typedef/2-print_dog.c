bd51dbff83b6f4bb45953e7e9e3864abb9ee6085
lincolnMK <kingsmkumbwa@gmail.com>
2023-01-16 04:31:02 -0800

    priting structures

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+/**
+ * print_dog - prints dog details strutre
+ * @d the struct type pointer to pbe printed
+ *
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		return;
+
+	}
+
+	if ((*d).name == NULL)
+		d->name = "(nil)";
+	if ((*d).owner == NULL)
+	{
+		d->owner = "(nil)";
+	}
+
+	printf("Name: %s\n", (*d).name);
+	printf("Age: %.1f\n", (*d).age);
+	printf("Owner: %s\n", (*d).owner);
+}
