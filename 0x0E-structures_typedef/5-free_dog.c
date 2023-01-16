da4051646ef3a00368574185750fac6428dbd25b
lincolnMK <kingsmkumbwa@gmail.com>
2023-01-16 05:38:08 -0800

    frrr

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+/**
+ * free_dog - frees mem allocated to dog if any
+ * @d: the space to be freeed
+ *
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free((*d).name);
+		free((*d).owner);
+		free(d);
+	}
+}
