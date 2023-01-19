7370624912a310c964b2be7434f29250f80c1223
lincolnMK <kingsmkumbwa@gmail.com>
2023-01-18 23:50:30 -0800

    iterate

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -7,8 +7,12 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int i, j, temp;
+	unsigned int i;
 
-
-
+	if (array == NULL || action == NULL)
+		return;
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
 }
