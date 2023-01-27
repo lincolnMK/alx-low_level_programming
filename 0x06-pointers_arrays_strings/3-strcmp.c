e591e90dc69f812674251ee969da8b8d81fd3cd9
lincolnMK <kingsmkumbwa@gmail.com>
2023-01-27 05:44:31 -0800

    3 challenge

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strcmp -compare string values given
+ * @s1: firs operando
+ * @s2: second operando
+ * Return: test result
+ */
+int _strcmp(cha *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+	while (s1[i] != '\0' && s2[i] = '\0')
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	return (0);
+}
