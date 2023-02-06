4b4a4139e99181cc7ef46f50c978f9fc2d22e4f2
lincolnMK <kingsmkumbwa@gmail.com>
2023-02-05 23:58:14 -0800

    pointers ee

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,22 +1,32 @@
 #include "function_pointers.h"
 #include <stdio.h>
+
 /**
- * int_index - search for int
- * @size: the number of elements in the array
- * @cmp: is a pointer to the functions to be used to compare values
- * @array: input arrays for the search
- * Return: value specified
+ * int_index - searches for an integer
+ *
+ * @array: int pointer
+ *
+ * @size: int
+ *
+ * @cmp: function pointer
+ *
+ * Return: void
  */
+
 int int_index(int *array, int size, int (*cmp)(int))
 {
+
 	int i;
 
-	if (array == NULL || size <= 0 || cmp == NULL)
-		return (-1);
-	for (i = 0; i < size; i++)
+	if (array && size && cmp)
 	{
-		if (cmp(array[i] != 0))
-			return (i);
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]))
+			{
+				return (i);
+			}
+		}
 	}
-	return (-1);
+		return (-1);
 }
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,8 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 #include <stdlib.h>
+
+int int_index(int *array, int size, int (*cmp)(int));
 void print_name(char *name, void  (*f)(char *));
 
 int _putchar (char c);
