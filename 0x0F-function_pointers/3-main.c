82b496d9d0c1d56604e637138c67e619682c3ca1
lincolnMK <kingsmkumbwa@gmail.com>
2023-02-06 00:41:54 -0800

    3rd stuff

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -8,32 +8,29 @@
  * @argv: arguments passed
  * @argc: number of arguments
  */
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	int a, b, ans;
-
-	int (*solve)(int, int);
-
-	char *get_op;
+	int result;
+	int (*calculator)(int, int);
 
 	if (argc != 4)
+	{
 		printf("Error\n");
 		exit(98);
-	
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
-	get_op = argv[2];
-
-	if (get_op_func(argv[2]) == NULL || argv[2][1] != '\0')
+	}
+	if (argv[2] == NULL || argv[2][1] != '\0')
+	{
 		printf("Error\n");
 		exit(99);
-	if ((*get_op == '/' || *get_op == ''%') && (*argv[3] == '0'))
+	}
+	calculator = get_op_func(argv[2]);
+
+	if (calculator == NULL)
+	{
 		printf("Error\n");
-		exit(100);
-
-	solve = get_op_func(get_op);
-	ans = solve(a, b);
-
-	printf("%d\n", ans);
+		exit(99);
+	}
+	result = calculator(atoi(argv[1]), atoi(argv[3]));
+	printf("%d\n", result);
 	return (0);
 }
